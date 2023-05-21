51c4e06d82582ad04a6e91d319c0f95d4db15827
jaimitin <jaimitin11@yahoo.com>
2023-05-20 22:52:02 -0400

    Add project files.

---
+++ EndlessCalculator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EndlessCalculator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
